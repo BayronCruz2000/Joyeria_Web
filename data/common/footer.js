893d7ee614a764a200b318302ea73abc6a462781
BayronCruz2000 <abrahamcruzm18@gmail.com>
2025-07-28 21:42:16 -0600

    Primer commit

---
+++ data/common/footer.js
@@ -0,0 +1,16 @@
+export const footer = {
+    footer : [
+        {
+            tittle: 'Para mas informacion llamar al',
+            content: '(+504) 8879-4123 o (+504) 9301-4107'
+        },
+        {
+            tittle: 'Direccion:',
+            content: 'Tegucigalpa, Francisco Morazan, Honduras'
+        },
+        {
+            tittle: 'Redes Sociales:',
+            content: 'Instagram: @JJasminneHN, Facebook: Joyeria y Accesorios Jasminne'
+        }
+    ]
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+import './Less/style.less';
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".carousel img");
+  let index = 0;
+
+  function showSlide(i) {
+    slides.forEach((slide, idx) => {
+      slide.classList.toggle("active", idx === i);
+    });
+  }
+
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  }
+
+  showSlide(index);
+  setInterval(nextSlide, 4000);
+});
